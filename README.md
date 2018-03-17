1fae8d2700689397acf399539db883e5f03dca2d
Costas Smaragdakis <kesmarag@tem.uoc.gr>
2018-03-17 16:26:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## installation using pip:
+
+pip install git+https://github.com/kesmarag/ml-gmm.git
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kesmarag-ml-gmm',
+      version='0.0.1',
+      description='Gaussian Mixture Model',
+      author='Costas Smaragdakis',
+      author_email='kesmarag@gmail.com',
+      url='https://github.com/kesmarag/ml-gmm',
+      packages=['kesmarag.ml.gmm'],
+      package_dir={'kesmarag.ml.gmm': './'},
+      install_requires=['numpy', 'scipy', 'sklearn', 'matplotlib'], )
